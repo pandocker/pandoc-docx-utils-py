f516525c62370fb6ff227551f1dfa9a90da65cb7
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2019-01-28 22:50:06 +0900

    add testing files

---
+++ test/test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from pandoc_docx_utils import ExtractBulletList
+import panflute as pf
+
+
+def main():
+    with open("markdown.md", "r") as f:
+        md = f.read()
+    doc = pf.Doc(*pf.convert_text(md), format="docx")
+    pf.debug(doc.content)
+    ebl = ExtractBulletList()
+    pf.run_filter(ebl.action, doc=doc)
+
+
+if __name__ == "__main__":
+    main()
