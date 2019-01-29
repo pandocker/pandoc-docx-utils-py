9a825e771f916a74474307b8584a3584e864dc2c
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2019-01-29 12:25:50 +0900

    [WIP] add Svg2Png class; add ExtractBulletList class

--- pandoc_docx_utils/__init__.py
+++ pandoc_docx_utils/__init__.py
@@ -9,7 +9,10 @@ pandoc -t docx -o <docx filename> <input filename>
 """
 
 import os
+import hashlib
 import panflute as pf
+import subprocess as sp
+from shutil import which
 
 
 class UnnumberHeadings(object):
@@ -63,6 +66,64 @@ class InlineFigureCentered(object):
                         subelem.attributes.pop("custom-style")
                         d = pf.Div(elem, attributes={"custom-style": style})
                         return d
+
+
+class Svg2Png(object):
+    def __init__(self):
+        self.dir_to = "svg"
+        assert which("rsvg-convert"), "rsvg-convert does not exist in path"
+
+    def action(self, elem, doc):
+        if doc.format not in ["html", "html5"]:
+            if isinstance(elem, pf.Image):
+                fn = elem.url
+                if fn.endswith(".svg"):
+                    counter = hashlib.sha1(fn.encode("utf-8")).hexdigest()[:8]
+
+                    if not os.path.exists(self.dir_to):
+                        pf.debug("mkdir")
+                        os.mkdir(self.dir_to)
+
+                    basename = "/".join([self.dir_to, str(counter)])
+
+                    if doc.format in ["latex"]:
+                        format = "pdf"
+                    else:
+                        format = "png"
+
+                    linkto = os.path.abspath(".".join([basename, format]))
+                    elem.url = linkto
+                    command = "rsvg-convert {} -f {} -o {}".format(fn, format, linkto)
+                    sp.Popen(command, shell=True, stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.PIPE)
+                    pf.debug("[inline] convert a svg file to {}".format(format))
+
+
+class ExtractBulletList(object):
+    """
+    Extracts bullet list to "Bullet List 1" and "Bullet List 2" styles
+    * "Bullet List 1" and "Bullet List 2" must be prepared in template
+    * Level-3 and lower lists are forced to be Level-2
+    """
+
+    def action(self, elem, doc):
+        if (doc.format == "docx"):
+            if isinstance(elem, pf.BulletList):
+                p = elem
+                depth = 0
+                pf.debug("elem: {}".format(elem))
+                while not isinstance(p, pf.Doc):
+                    if isinstance(p, pf.ListItem):
+                        pf.debug("content: {}".format(*p.content))
+                        depth += 1
+                    p = p.parent
+                pf.debug(depth)
+
+                # if depth >= 2:
+                #     d = pf.Div(*elem.content, attributes={"custom-style": "Bullet List 2"})
+                # else:
+                #     d = pf.Div(*elem.content, attributes={"custom-classes": "Bullet List 1"})
+                # pf.debug(d.attributes)
+                return []
 
 
 def main(doc=None):
